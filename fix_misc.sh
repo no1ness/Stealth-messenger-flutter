9755f668a6ae8185b3a64d109ec3d9687fbfabb5
anikakle <nooness@gmail.com>
2026-06-22 18:54:55 +0300

    chore: cleanup temp scripts, update .gitignore

--- fix_misc.sh
+++
@@ -1,3 +0,0 @@
-sed -i 's/AppFontStacks.monoFallbacks/["monospace"]/g' /home/ruslan/projects/STEALTH/client/lib/ui/screens/loading_screen.dart
-sed -i 's/DialogImportance\./FlatIntensity./g' /home/ruslan/projects/STEALTH/client/lib/ui/widgets/call_manager.dart /home/ruslan/projects/STEALTH/client/lib/ui/widgets/chat_bubble.dart
-echo fixed_remaining
